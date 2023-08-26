a53e970692e2dca709792fd5ee481f0f43d214e7
MdodoIng <mdodoyt2@gmail.com>
2023-08-26 16:40:28 +0530

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,13 @@
+"use client";
+import { Overlay } from "@/components/overlay";
+import { Webgl } from "@/components/webgl";
+import React from "react";
+
+const Home = () => (
+  <>
+    <Webgl />
+    <Overlay />
+  </>
+);
+
+export default Home;
